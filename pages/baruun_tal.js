f2bfef1344502cc29223b7337f8bdc7cde610c3f
otgonbat <otgonbat20@gmail.com>
2022-10-19 15:30:45 +0800

    pushed by otgonbat

---
+++ pages/baruun_tal.js
@@ -0,0 +1,10 @@
+export default function baruun_tal() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome back</h1>
+        <p>Welcome back! Please enter your details</p>
+      </div>
+    </div>
+  );
+}
